f4b5feea1b1a777bafcaf1e1c1dfa131ae5cc93f
LP <2200187107@qq.com>
2025-04-05 16:27:10 +0800

    feat: :sparkles: 增加中线绘制

--- user/cpu2_main.c
+++ user/cpu2_main.c
@@ -1,37 +1,37 @@
 /*********************************************************************************************************************
-* TC377 Opensourec Library 即（TC377 开源库）是一个基于官方 SDK 接口的第三方开源库
-* Copyright (c) 2022 SEEKFREE 逐飞科技
-*
-* 本文件是 TC377 开源库的一部分
-*
-* TC377 开源库 是免费软件
-* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
-* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
-*
-* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
-* 甚至没有隐含的适销性或适合特定用途的保证
-* 更多细节请参见 GPL
-*
-* 您应该在收到本开源库的同时收到一份 GPL 的副本
-* 如果没有，请参阅<https://www.gnu.org/licenses/>
-*
-* 额外注明：
-* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
-* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
-* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
-* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
-*
-* 文件名称          cpu2_main
-* 公司名称          成都逐飞科技有限公司
-* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
-* 开发环境          ADS v1.10.2
-* 适用平台          TC377TP
-* 店铺链接          https://seekfree.taobao.com/
-*
-* 修改记录
-* 日期              作者                备注
-* 2022-11-03       pudding            first version
-********************************************************************************************************************/
+ * TC377 Opensourec Library 即（TC377 开源库）是一个基于官方 SDK 接口的第三方开源库
+ * Copyright (c) 2022 SEEKFREE 逐飞科技
+ *
+ * 本文件是 TC377 开源库的一部分
+ *
+ * TC377 开源库 是免费软件
+ * 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+ * 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
+ *
+ * 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+ * 甚至没有隐含的适销性或适合特定用途的保证
+ * 更多细节请参见 GPL
+ *
+ * 您应该在收到本开源库的同时收到一份 GPL 的副本
+ * 如果没有，请参阅<https://www.gnu.org/licenses/>
+ *
+ * 额外注明：
+ * 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+ * 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+ * 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+ * 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
+ *
+ * 文件名称          cpu2_main
+ * 公司名称          成都逐飞科技有限公司
+ * 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+ * 开发环境          ADS v1.10.2
+ * 适用平台          TC377TP
+ * 店铺链接          https://seekfree.taobao.com/
+ *
+ * 修改记录
+ * 日期              作者                备注
+ * 2022-11-03       pudding            first version
+ ********************************************************************************************************************/
 #include "zf_common_headfile.h"
 #include "motor.h"
 #include "image.h"
@@ -46,74 +46,82 @@
 // **************************** 代码区域 ****************************
 void show_star(uint8 x, uint8 y)
 {
-   if((x>1)&&(x<ips200_width_max)&&(y>1)&&(y<ips200_height_max))
-   ips200_draw_point((uint16)x, (uint16)y, uesr_GREEN);//角点曲率半径小（包括边缘断线处+岔道处）
-   ips200_draw_point((uint16)x+1, (uint16)y, uesr_GREEN);
-   ips200_draw_point((uint16)x, (uint16)y+1, uesr_GREEN);
-   ips200_draw_point((uint16)x-1, (uint16)y, uesr_GREEN);
-   ips200_draw_point((uint16)x, (uint16)y-1, uesr_GREEN);
+    if ((x > 1) && (x < ips200_width_max) && (y > 1) && (y < ips200_height_max))
+        ips200_draw_point((uint16)x, (uint16)y, uesr_GREEN); // 角点曲率半径小（包括边缘断线处+岔道处）
+    ips200_draw_point((uint16)x + 1, (uint16)y, uesr_GREEN);
+    ips200_draw_point((uint16)x, (uint16)y + 1, uesr_GREEN);
+    ips200_draw_point((uint16)x - 1, (uint16)y, uesr_GREEN);
+    ips200_draw_point((uint16)x, (uint16)y - 1, uesr_GREEN);
 }
 void screen_show(void)
 {
-    //此处展示屏幕
-   ips200_show_gray_image(0+image_xmove, 0, original_image[0], image_w, image_h, image_w, image_h, 0);
-   ips200_show_gray_image(0+image_xmove, 125, bin_image[0], image_w, image_h, image_w, image_h, 0);
-//
-//    ips200_show_string(0, 180, "ifstop:");
-//    ips200_show_int(60,180,stop,3);
-//
-//    ips200_show_string(0, 200, "lspeed:");
-//    ips200_show_int(60,200,speed_l,3);
-//    ips200_show_string(100, 200, "rspeed:");
-//    ips200_show_int(160,200,speed_r,3);
-//
-//    ips200_show_string(0, 220, "error:");
-//    ips200_show_int(60,220,error,3);
-//    ips200_show_string(100, 220, "dspeed:");
-//    ips200_show_int(160,220,d_speed,3);
-//
-//    ips200_show_int(0,260,head_roadwidth,3);
-//    ips200_show_int(40,260,foot_roadwidth,3);
-//    if(xflg_now > 3)//xflg_now>3即三次连续超标，表明底线丢线
-//        {
-//            ips200_show_string(80, 260, "losfoot");
-//        }
-//        else
-//        {
-//        //底边未丢线，分辨前方是否出现丢线
-//            ips200_show_string(80, 260, "getfoot");
-//        }
-    ips200_draw_line(0+image_xmove, (uint16)hightest, image_w+image_xmove, (uint16)hightest, RGB565_RED);
-    ips200_draw_line(0+image_xmove, (uint16)hightest+125, image_w+image_xmove, (uint16)hightest+125, RGB565_RED);
-    ips200_draw_line(0+image_xmove, (uint16)image_h-lowest, image_w+image_xmove, (uint16)image_h-lowest, RGB565_RED);
-    ips200_draw_line(0+image_xmove, (uint16)image_h-lowest+125, image_w+image_xmove, (uint16)image_h-lowest+125, RGB565_RED);
-    for (int i = hightest; i < image_h-1; i++)
+    // 此处展示屏幕
+    ips200_show_gray_image(0 + image_xmove, 0, original_image[0], image_w, image_h, image_w, image_h, 0);
+    ips200_show_gray_image(0 + image_xmove, 125, bin_image[0], image_w, image_h, image_w, image_h, 0);
+    //
+    //    ips200_show_string(0, 180, "ifstop:");
+    //    ips200_show_int(60,180,stop,3);
+    //
+    //    ips200_show_string(0, 200, "lspeed:");
+    //    ips200_show_int(60,200,speed_l,3);
+    //    ips200_show_string(100, 200, "rspeed:");
+    //    ips200_show_int(160,200,speed_r,3);
+    //
+    //    ips200_show_string(0, 220, "error:");
+    //    ips200_show_int(60,220,error,3);
+    //    ips200_show_string(100, 220, "dspeed:");
+    //    ips200_show_int(160,220,d_speed,3);
+    //
+    //    ips200_show_int(0,260,head_roadwidth,3);
+    //    ips200_show_int(40,260,foot_roadwidth,3);
+    //    if(xflg_now > 3)//xflg_now>3即三次连续超标，表明底线丢线
+    //        {
+    //            ips200_show_string(80, 260, "losfoot");
+    //        }
+    //        else
+    //        {
+    //        //底边未丢线，分辨前方是否出现丢线
+    //            ips200_show_string(80, 260, "getfoot");
+    //        }
+    ips200_draw_line(0 + image_xmove, (uint16)hightest, image_w + image_xmove, (uint16)hightest, RGB565_RED);
+    ips200_draw_line(0 + image_xmove, (uint16)hightest + 125, image_w + image_xmove, (uint16)hightest + 125, RGB565_RED);
+    ips200_draw_line(0 + image_xmove, (uint16)image_h - lowest, image_w + image_xmove, (uint16)image_h - lowest, RGB565_RED);
+    ips200_draw_line(0 + image_xmove, (uint16)image_h - lowest + 125, image_w + image_xmove, (uint16)image_h - lowest + 125, RGB565_RED);
+    for (int i = hightest; i < image_h - 1; i++)
     {
-        if((l_border[i] > 0)&&((l_border[i]+image_xmove)<(ips200_width_max-1))){
-        ips200_draw_point((uint16)l_border[i]+image_xmove, (uint16)(i), uesr_RED); /*左线*/
-        ips200_draw_point((uint16)l_border[i]+1+image_xmove, (uint16)(i), uesr_RED);
-        ips200_draw_point((uint16)l_border[i]-1+image_xmove, (uint16)(i), uesr_RED);
+        if ((l_border[i] > 0) && ((l_border[i] + image_xmove) < (ips200_width_max - 1)))
+        {
+            ips200_draw_point((uint16)l_border[i] + image_xmove, (uint16)(i), uesr_RED); /*左线*/
+            ips200_draw_point((uint16)l_border[i] + 1 + image_xmove, (uint16)(i), uesr_RED);
+            ips200_draw_point((uint16)l_border[i] - 1 + image_xmove, (uint16)(i), uesr_RED);
         }
-        if((r_border[i] > 0)&&((r_border[i]+image_xmove)<(ips200_width_max-1))){
-        ips200_draw_point((uint16)r_border[i]+image_xmove, (uint16)(i), uesr_RED); /*右线*/
-        ips200_draw_point((uint16)r_border[i]+1+image_xmove, (uint16)(i), uesr_RED);
-        ips200_draw_point((uint16)r_border[i]-1+image_xmove, (uint16)(i), uesr_RED);
+        if ((r_border[i] > 0) && ((r_border[i] + image_xmove) < (ips200_width_max - 1)))
+        {
+            ips200_draw_point((uint16)r_border[i] + image_xmove, (uint16)(i), uesr_RED); /*右线*/
+            ips200_draw_point((uint16)r_border[i] + 1 + image_xmove, (uint16)(i), uesr_RED);
+            ips200_draw_point((uint16)r_border[i] - 1 + image_xmove, (uint16)(i), uesr_RED);
+        }
+        if ((center_line[i] > 0) && ((center_line[i] + image_xmove) < (ips200_width_max - 1)))
+        {
+            ips200_draw_point((uint16)center_line[i] + image_xmove, (uint16)(i), uesr_BLUE); /*右线*/
+            ips200_draw_point((uint16)center_line[i] + 1 + image_xmove, (uint16)(i), uesr_BLUE);
+            ips200_draw_point((uint16)center_line[i] - 1 + image_xmove, (uint16)(i), uesr_BLUE);
         }
     }
 }
-void core2_main(void)//负责屏幕显示
+void core2_main(void) // 负责屏幕显示
 {
-    disable_Watchdog();                     // 关闭看门狗
-    interrupt_global_enable(0);             // 打开全局中断
+    disable_Watchdog();         // 关闭看门狗
+    interrupt_global_enable(0); // 打开全局中断
     // 此处编写用户代码 例如外设初始化代码等
     // 此处编写用户代码 例如外设初始化代码等
-    cpu_wait_event_ready();                 // 等待所有核心初始化完毕
+    cpu_wait_event_ready(); // 等待所有核心初始化完毕
     while (TRUE)
     {
         // 此处编写需要循环执行的代码
         if (IfxCpu_acquireMutex(&screen_mutex))
         {
-           screen_show();
+            screen_show();
             IfxCpu_releaseMutex(&screen_mutex);
         }
         system_delay_ms(50);
@@ -121,7 +129,5 @@ void core2_main(void)//负责屏幕显示
         // 此处编写需要循环执行的代码
     }
 }
-
-
 
 #pragma section all restore
